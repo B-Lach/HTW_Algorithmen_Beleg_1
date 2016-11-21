b6c06a349041a5993af47f2190df11b0683c85e4
Benny Lach <benny@discollect.me>
2016-11-21 19:23:37 +0100

    Added setup function to the JUnit test for the Student class

--- src/data/InitTest.java
+++ src/data/InitTest.java
@@ -2,18 +2,22 @@ package data;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class InitTest {
-
-	@Test
-	public void test_initializer() {
-		Student student = new Student("Hans", "Wurst", 555568, Courses.AI);
-		assertEquals("Hans", student.getForname());
-		assertEquals("Wurst", student.getSurname());
-		assertEquals(555568, student.getId());
-		assertEquals(Courses.AI, student.getCourse());
-				
+	private Student student;
+	
+	@Before
+    public void init() {
+		student = new Student("Hans", "Wurst", 555568, Courses.AI);
 	}
-
+	
+	@Test
+	public void test_initializer() {	
+		assertEquals("fornames are not equal", "Hans", student.getForname());
+		assertEquals("surnames are not equal", "Wurst", student.getSurname());
+		assertEquals("Ids are not equal", 555568, student.getId());
+		assertEquals("Courses are not equal", Courses.AI, student.getCourse());
+	}
 }
