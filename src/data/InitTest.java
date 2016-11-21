0cffcf2be14a876fc56926545accf1e667aa15df
Benny Lach <benny@discollect.me>
2016-11-21 19:05:14 +0100

    Updated test

--- src/data/InitTest.java
+++ src/data/InitTest.java
@@ -9,10 +9,10 @@ public class InitTest {
 	@Test
 	public void test_initializer() {
 		Student student = new Student("Hans", "Wurst", 555568, Courses.AI);
-		assert("Hans",student.firstname);
-		assert("Wurst", student.surname);
-		assert(555568, student.id);
-		assert(Courses.AI, student.course);
+		assertEquals("Hans", student.getForname());
+		assertEquals("Wurst", student.getSurname());
+		assertEquals(555568, student.getId());
+		assertEquals(Courses.AI, student.getCourse());
 				
 	}
 
